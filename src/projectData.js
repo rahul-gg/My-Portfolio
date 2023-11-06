9b10bc226b79775679dc2c324f10df4c63405bd1
ziggler33 <rohan141166@gmail.com>
2023-11-06 12:58:12 +0530

    Made mobile responsive

--- src/projectData.js
+++ src/projectData.js
@@ -4,7 +4,10 @@ const projectData=[
         projectTitle: "Admin Dash Board",
         projectType: "Full Stack",
         projectDesc: "A full stack admin dashoboard that can be customized to visualize data from a database",
-        projectTags: ["React.js", "Node.js", "Express.js", "MongoDB", "CSS", "D3.js","RESTAPI"],
+        projectTags: [
+            {id:1,value:"React.js"},{id:2,value:"Node.js"}, {id:3,value:"Express.js"} , {id:4,value:"MongoDB"}, {id:5,value:"CSS"}, {id:6,value:"D3.js"}, {id:7,value:"RESTAPI"}
+        
+        ],
         projectLink: "https://github.com/rahul-gg"
     },
 
@@ -13,7 +16,10 @@ const projectData=[
         projectTitle: "Netflix UI Clone",
         projectType: "FrontEnd",
         projectDesc: "A dynamic clone of Netflix's UI made by consuming a REST API to get the updated data on Netflix",
-        projectTags: ["JavaScript", "RESTAPI", "HTML", "CSS"],
+        projectTags:[
+            {id:1,value:"JavaScript"},{id:2,value:"HTML"}, {id:3,value:"CSS"} , {id:4,value:"RESTAPI"}
+        ],
+        //projectTags: ["JavaScript", "RESTAPI", "HTML", "CSS"],
         projectLink: "https://github.com/rahul-gg"
     },
 
@@ -22,7 +28,10 @@ const projectData=[
         projectTitle: "Burger PLace",
         projectType: "Full Stack",
         projectDesc: "An e-commerce site for selling burgers with authentication implemented",
-        projectTags: ["React.js", "Jest", "Enzyme", "Redux","JavaScript","CSS"],
+       // projectTags: ["React.js", "Jest", "Enzyme", "Redux","JavaScript","CSS"],
+       projectTags:[
+        {id:1,value:"React.js"},{id:2,value:"Jest"}, {id:3,value:"Enzyme"} , {id:4,value:"Redux"},{id:5,value:"JavaScript"} , {id:6,value:"CSS"}
+    ],
         projectLink: "https://react-myburger-07.firebaseapp.com/"
     }
 
